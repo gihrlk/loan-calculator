dac24026a1e8eacc781985d276a580e9dce872d4
Gihan Rajakaruna <gihan@cohga.com>
2021-10-18 15:33:50 +1100

    Minor bug fixes.

--- src/main/java/com/challenge/lc/exception/PaymentAmountNotValidException.java
+++ src/main/java/com/challenge/lc/exception/PaymentAmountNotValidException.java
@@ -5,7 +5,7 @@ public class PaymentAmountNotValidException extends RuntimeException {
 	private static final long serialVersionUID = 6484020025523880923L;
 
 	public PaymentAmountNotValidException() {
-		super("Loan not found.");
+		super("Incorrect payment.");
 	}
 
 	public PaymentAmountNotValidException(String message) {
